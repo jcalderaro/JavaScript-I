87797e9f7db1506a83e5f1fc7c2aad7ca0fb9398
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-04-15 13:40:49 -0700

    Starting Function Part 1

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,40 @@
 // };
 // myFunction();
 
+let myFunction = function () {
+console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = function (param) {
+    return param;
+};
+anotherFunction("Example");
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = function (param1, param2) {
+    return param1 + param2;
+};
+add(1,2);
+
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
-
-// Stretch
-
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+let subtract = function (param1, param2) {
+    return param1 - param2;
+};
+subtract(1,2);
